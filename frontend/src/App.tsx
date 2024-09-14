716a5525a03ba276ea0bf936273ee8184df08590
Sabine Sun <sabine.sun@hotmail.com>
2024-09-14 16:35:31 +0200

    refactor: move files to frontend directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className="flex h-screen w-screen items-center justify-center">
-      <h1 className="text-7xl font-bold">Hello World !</h1>
+      <h1 className="text-7xl font-bold">Hello, World!</h1>
     </main>
   );
 }
