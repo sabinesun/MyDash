ddabdf43de83d942437424b89c46f4016785f23c
Sabine Sun <sabine.sun@hotmail.com>
2024-09-23 13:44:58 +0200

    fix: card font size

--- frontend/src/components/card/card-content.tsx
+++ frontend/src/components/card/card-content.tsx
@@ -7,12 +7,12 @@ type CardProps = {
 
 export const CardContent = ({ label, icon, value, unit }: CardProps) => {
     return (
-        <div className="flex w-full flex-col gap-2 rounded-md border border-primary bg-white p-8">
+        <div className="flex w-full flex-col gap-2 rounded-md border border-primary bg-white p-3 lg:p-8">
             <div className="flex gap-2">
                 {icon}
-                <h2 className="text-lg">{label}</h2>
+                <h2 className="text-base lg:text-lg">{label}</h2>
             </div>
-            <p className="flex justify-end text-2xl">
+            <p className="flex justify-end lg:text-2xl text-lg">
                 {label === "Total Revenue" ? Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(Number(value)) : value.toLocaleString()}
                 {unit}
             </p>
